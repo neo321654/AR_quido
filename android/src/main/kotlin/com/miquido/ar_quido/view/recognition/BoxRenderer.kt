61318e4bce7dceaa5a1656531ceea46df2549bde
neo321654 <neo321654@rambler.ru>
2024-02-14 13:52:55 +0300

    222

--- android/src/main/kotlin/com/miquido/ar_quido/view/recognition/BoxRenderer.kt
+++ android/src/main/kotlin/com/miquido/ar_quido/view/recognition/BoxRenderer.kt
@@ -43,7 +43,7 @@ class BoxRenderer {
         + "\n")
 
     init {
-        val boxColor = floatArrayOf(0.1f, 0.8f, 1f, 0.4f)
+        val boxColor = floatArrayOf(0.1f, 0.1f, 1f, 0.2f)
         currentContext = (EGLContext.getEGL() as EGL10).eglGetCurrentContext()
         programBox = GLES30.glCreateProgram()
         val vertShader = GLES30.glCreateShader(GLES30.GL_VERTEX_SHADER).also {
